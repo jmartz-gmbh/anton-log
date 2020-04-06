fba88357f73f2564db1f5730aee6479caac3bde1
Jonathan Martz <git@jmartz.de>
2020-04-06 20:04:47 +0200

    add logic for pipeline and timestamp for logs

--- src/Log.php
+++ src/Log.php
@@ -7,6 +7,8 @@ class Log{
 
     public $project = '';
 
+    public $pipeline = '';
+
     public $data = [];
 
     public $message = [];
@@ -19,8 +21,12 @@ class Log{
         $this->project = $project;
     }
 
+    public function setPipeline(string $pipeline){
+        $this->pipeline = $pipeline;
+    }
+
     public function addMessage(array $message){
-        $this->message[] = $message;
+        $this->message[] = date('d.m.Y H:i:s').': '.$message;
     }
 
     public function addData(array $data){
@@ -30,6 +36,9 @@ class Log{
     public function save(){
         if(empty($this->project)){
             die('Log has no Project Name! ('.$this->filename.')');
+        }
+        if(empty($this->pipeline)){
+            die('Log has no Pipeline! ('.$this->project.')');
         }
         exec('mkdir -p storage/logs/'.$this->project);
         file_put_contents($this->filename, jsone_encode([$this->message, $this->data], JSON_FORCE_OBJECT));
